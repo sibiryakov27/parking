4eb8f8e28d9990278ab39f5d50bdcb9ef1fd4303
Ivan Sibiryakov <ivan.sibiryakov27@gmail.com>
2022-10-04 14:19:24 +0300

    Add post method for adding new booking node

--- src/main/java/com/sigma/parking/controller/ParkingController.java
+++ src/main/java/com/sigma/parking/controller/ParkingController.java
@@ -4,9 +4,12 @@ import com.sigma.parking.data.dto.BookingDTO;
 import com.sigma.parking.service.ParkingService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,5 +22,10 @@ public class ParkingController {
     @GetMapping("/booking")
     public List<BookingDTO> getAllBookings() {
         return parkingService.getAllBookings();
+    }
+
+    @PostMapping("/booking")
+    public BookingDTO addNewBooking(@Valid @RequestBody BookingDTO newBooking) {
+        return parkingService.saveBooking(newBooking);
     }
 }
--- src/main/java/com/sigma/parking/service/ParkingService.java
+++ src/main/java/com/sigma/parking/service/ParkingService.java
@@ -2,12 +2,17 @@ package com.sigma.parking.service;
 
 import com.sigma.parking.data.dto.BookingDTO;
 import com.sigma.parking.data.entity.BookingEntity;
+import com.sigma.parking.data.entity.CarEntity;
+import com.sigma.parking.data.entity.ParkingSpaceEntity;
 import com.sigma.parking.repository.BookingRepository;
+import com.sigma.parking.repository.CarRepository;
+import com.sigma.parking.repository.ParkingSpaceRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,10 +20,26 @@ import java.util.stream.Collectors;
 public class ParkingService {
 
     private BookingRepository bookingRepository;
+    private CarRepository carRepository;
+    private ParkingSpaceRepository parkingSpaceRepository;
     private ModelMapper modelMapper;
 
     public List<BookingDTO> getAllBookings() {
         List<BookingEntity> bookings = bookingRepository.findAll();
         return bookings.stream().map(entity -> modelMapper.map(entity, BookingDTO.class)).collect(Collectors.toList());
+    }
+
+    public BookingDTO saveBooking(BookingDTO booking) {
+        BookingEntity bookingEntity = modelMapper.map(booking, BookingEntity.class);
+        Optional<CarEntity> car = carRepository.findByCarNumber(booking.getCar().getCarNumber());
+        Optional<ParkingSpaceEntity> parkingSpace = parkingSpaceRepository.findById(booking.getParkingSpace().getId());
+        if (car.isPresent()) {
+            bookingEntity.setCar(car.get());
+        }
+        if (parkingSpace.isPresent()) {
+            bookingEntity.setParkingSpace(parkingSpace.get());
+        }
+        bookingEntity = bookingRepository.save(bookingEntity);
+        return modelMapper.map(bookingEntity, BookingDTO.class);
     }
 }
